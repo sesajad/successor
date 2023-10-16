05ce4e67297bcc683e74a6eb97151a2ee3fc5697
Sajad <sska1377@gmail.com>
2023-10-16 23:51:53 +0100

    update installer script

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 ARCH=$(uname -m)
 VERSION=0.1.0
 
-wget https://github.com/sesajad/successor/releases/download/v${VERSION}/successor-${ARCH}.tar.gz -o /tmp/succ.tar.gz
-mkdir /tmp/succ
-tar -xzf /tmp/succ.tar.gz -C /tmp/succ
-mkdir /succ
-mkdir /succ/bin
-mv /sbin/init /sbin/init2
-cp /tmp/succ/ /succ/bin/
-ln -s /succ/bin/init /sbin/init
+wget https://github.com/sesajad/successor/releases/download/v${VERSION}/successor-${ARCH}.tar.gz -o /tmp/succ.tar.gz && \
+mkdir /tmp/succ && \
+tar -xzf /tmp/succ.tar.gz -C /tmp/succ && \
+mkdir /succ && \
+mkdir /succ/bin && \
+mv /sbin/init /sbin/init2 && \
+cp /tmp/succ/ /succ/bin/ && \
+ln -s /succ/bin/init /sbin/init && \
 rm -rf /tmp/succ
