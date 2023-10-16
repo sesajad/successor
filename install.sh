a23b47b21aade336dcbd5bd47abbcce22288dcb0
Sajad <sska1377@gmail.com>
2023-10-16 23:59:27 +0100

    improve installer

--- install.sh
+++ install.sh
@@ -10,6 +10,11 @@ rm -rf /tmp/succ.tar.gz && \
 mkdir /succ && \
 mkdir /succ/bin && \
 mv /sbin/init /sbin/init2 && \
-cp /tmp/succ/ /succ/bin/ && \
+cp /tmp/succ/init /succ/bin/init && \
+cp /tmp/succ/succ-build /succ/bin/succ-build && \
+cp /tmp/succ/succ-clean /succ/bin/succ-clean && \
+cp /tmp/succ/succ-current /succ/bin/succ-current && \
+cp /tmp/succ/succ-uninstall /succ/bin/succ-uninstall && \
+chmod +x /succ/bin/* && \
 ln -s /succ/bin/init /sbin/init && \
 rm -rf /tmp/succ && \
