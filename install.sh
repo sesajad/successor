a9ceb1957790fac2113e3fd1d09d960f5821910c
Sajad <sska1377@gmail.com>
2023-10-16 23:56:16 +0100

    fix installer script

--- install.sh
+++ install.sh
@@ -3,12 +3,13 @@
 ARCH=$(uname -m)
 VERSION=0.1.0
 
-wget https://github.com/sesajad/successor/releases/download/v${VERSION}/successor-${ARCH}.tar.gz -o /tmp/succ.tar.gz && \
+wget https://github.com/sesajad/successor/releases/download/v${VERSION}/successor-${ARCH}.tar.gz -O /tmp/succ.tar.gz && \
 mkdir /tmp/succ && \
 tar -xzf /tmp/succ.tar.gz -C /tmp/succ && \
+rm -rf /tmp/succ.tar.gz && \
 mkdir /succ && \
 mkdir /succ/bin && \
 mv /sbin/init /sbin/init2 && \
 cp /tmp/succ/ /succ/bin/ && \
 ln -s /succ/bin/init /sbin/init && \
-rm -rf /tmp/succ
+rm -rf /tmp/succ && \
