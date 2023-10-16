8ceda4ccde9e80fabd552426c67291e0ffda2952
Sajad <sska1377@gmail.com>
2023-10-16 23:37:04 +0100

    fix download url

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ARCH=$(uname -m)
 VERSION=0.1.0
-wget https://github.com/sesajad/successor/releases/download/v${VERSION}/successor-${VERSION}-${ARCH}.tar.gz -o /tmp/succ.tar.gz
+wget https://github.com/sesajad/successor/releases/download/v${VERSION}/successor-${ARCH}.tar.gz -o /tmp/succ.tar.gz
 
 tar -xzf /tmp/succ.tar.gz -C /tmp/succ
 mkdir /succ
