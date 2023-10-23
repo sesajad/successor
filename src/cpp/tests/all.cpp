8bccdefc6f54132a667fe190bc0ec91820e5c741
Sajad <sska1377@gmail.com>
2023-10-23 14:16:36 +0100

    add tests

---
+++ src/cpp/tests/all.cpp
@@ -0,0 +1,1 @@
+#include "unit_system.hpp"
---
+++ src/cpp/tests/unit_system.hpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE system
+#include <boost/test/included/unit_test.hpp>
+
+#include "../interfaces/system.hpp"
+
+BOOST_AUTO_TEST_CASE(test_execute)
+{
+  BOOST_CHECK_EQUAL(sys::execute("echo", {"hello"}, false, true), 0);
+  BOOST_CHECK_NE(sys::execute("cat", {"/nonexistent"}, false, true), 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_binary_exists)
+{
+  BOOST_CHECK(sys::binary_exists("cat"));
+  BOOST_CHECK(!sys::binary_exists("nonexistent"));
+}
